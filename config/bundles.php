06d886d59a04e84a0127d0205fd63d909d55f115
Malte Hübner <maltehuebner@gmx.org>
2024-11-20 20:19:43 +0100

    Getting started with webpack encore.

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
